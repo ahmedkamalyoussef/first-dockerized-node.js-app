6fc11f759a80b0b6d420187e47cdeb4bee316680
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2024-10-20 04:24:13 +0300

    using redis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const mongoose=require('mongoose');
+const redis =require("redis");
 const app = express();
+
+const redisClient =redis.createClient({
+  url:'redis://redis:6379'
+});
+redisClient.on("error",(err)=>console.log('failed to connect to redis',err));
+redisClient.on("connect",()=>console.log('connected to redis '));
+redisClient.connect();
+
 
 const DB_USER='root';
 const DB_PASSWORD ='example';
@@ -12,10 +21,12 @@ mongoose.connect(URI).then(()=>{console.log("connected successfully")}).catch((e
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World asdasd!');
-});
-app.get('/t', (req, res) => {
+  redisClient.set("products","products ...")
   res.send('Hello, World!');
+});
+app.get('/data',async (req, res) => {
+  const products=await redisClient.get("products")
+  res.send(products);
 });
 
 app.listen(PORT, () => {
